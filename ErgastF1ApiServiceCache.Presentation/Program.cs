caa3094430c5a67571245d8ca74477e06919ed4c
pablosilvamachado <pablosilvamachado@hotmal.com>
2024-12-19 12:31:41 -0300

    Feat - Add Swagger.

--- ErgastF1ApiServiceCache.Presentation/Program.cs
+++ ErgastF1ApiServiceCache.Presentation/Program.cs
@@ -5,8 +5,12 @@ using ErgastF1ApiServiceCache.Domain.Interfaces.Repositories;
 using ErgastF1ApiServiceCache.Infrastructure.Cache;
 using ErgastF1ApiServiceCache.Infrastructure.Repositories;
 using StackExchange.Redis;
+using Microsoft.OpenApi.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 // Configuração do Redis
 builder.Services.AddSingleton<IConnectionMultiplexer>(_ =>
@@ -25,10 +29,59 @@ builder.Services.AddScoped<ISeasonRepository, SeasonRepository>();
 
 // Controllers
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Formula 1 API",
+        Version = "v1",
+        Description = "API para obter dados relacionados à Fórmula 1, atualmente suportando Seasons.",
+        Contact = new OpenApiContact
+        {
+            Name = "Pablo da Silva mMachado",
+            Email = "pablosilvamachado@hotmail.com",
+            Url = new Uri("https://github.com/pablosilvamachado")
+        }
+    });
+});
+
+
+string[] origens = ["http://localhost:4200", "https://localhost:4200", "http://localhost:8080", "http://localhost:8081", "http://localhost:5248"];
+
+builder.Services.AddCors(
+                options => options.AddPolicy(
+                    MyAllowSpecificOrigins,
+                    builder => builder
+                        .WithOrigins(origens)
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials()
+                )
+            );
 
 var app = builder.Build();
 
+
+// Configura o middleware do Swagger
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Formula 1 API v1");
+        c.RoutePrefix = string.Empty; 
+    });
+}
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.UseAuthorization();
+
+app.UseHttpsRedirection();
+
 app.UseRouting();
+
 app.MapControllers();
 
 app.Run();
